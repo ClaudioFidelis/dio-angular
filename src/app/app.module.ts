2fc47feb7582a0c28bbf25ac714aad87ca42988f
Claudio <claudiofazevedo@gmail.com>
2022-06-14 20:19:13 -0300

    Adicionando rota, redirecionando e chamando componente para a SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // importando forms module
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
@@ -21,7 +22,15 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
     BrowserModule,
     // importando forms module
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
