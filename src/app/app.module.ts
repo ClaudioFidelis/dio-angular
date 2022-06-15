73e601d533292786c5df6ff92083f23cb3b3fd05
Claudio <claudiofazevedo@gmail.com>
2022-06-14 22:12:45 -0300

    Página de detalhes passando ID selecionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StarComponent } from './star/star.component';
 import { replacePipe }  from  './pipe/replace.pipe'
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { Error404Component } from './error-404/error-404.component';
     replacePipe,
     NavBarComponent,
     Error404Component,
+    CourseInfoComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,10 +29,13 @@ import { Error404Component } from './error-404/error-404.component';
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full'
+        path: 'courses', component: CourseListComponent
       },
       {
-        path: 'courses', component: CourseListComponent
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
         path: '**', component: Error404Component
